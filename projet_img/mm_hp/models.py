a405c444fe4642942e417cfa30d0d80887ef339e
hugo.pradier2 <hugo.pradier2@etu.uca.fr>
2024-02-16 19:50:58 +0100

    ajout des fonctionnalités pour deux images

--- projet_img/mm_hp/models.py
+++ projet_img/mm_hp/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class ImageModel(models.Model):
     original_image1 = models.ImageField(upload_to='original/')
     original_image2 = models.ImageField(upload_to='original/')
-    black_and_white_image = models.ImageField(upload_to='blackAndWhite/', blank=True, null=True)
-    resized_image = models.ImageField(upload_to='resize/', blank=True, null=True)
-    grayscale_image = models.ImageField(upload_to='grayscale/', blank=True, null=True)
+    black_and_white_image1 = models.ImageField(upload_to='blackAndWhite/', blank=True, null=True)
+    black_and_white_image2 = models.ImageField(upload_to='blackAndWhite/', blank=True, null=True)
+    resized_image1 = models.ImageField(upload_to='resized/', blank=True, null=True)
+    resized_image2 = models.ImageField(upload_to='resized/', blank=True, null=True)
+    grayscale_image1 = models.ImageField(upload_to='grayscale/', blank=True, null=True)
+    grayscale_image2 = models.ImageField(upload_to='grayscale/', blank=True, null=True)
     merged_image = models.ImageField(upload_to='merged/', blank=True, null=True)
--- projet_img/mm_hp/views.py
+++ projet_img/mm_hp/views.py
@@ -38,78 +38,126 @@ def process_image(request, image_id):
         elif action == 'merge':
             merge_images(image)
 
+        # Rafraîchir les données de l'objet image depuis la base de données après les modifications
         image.refresh_from_db()
 
+    # Envoyer le contexte mis à jour au template
     return render(request, 'process_image.html', {'image': image, 'form': ImageForm()})
 
-def apply_black_and_white(image_obj):
-    with open(image_obj.original_image1.path, 'rb') as f:
-        original_image1 = Image.open(f)
-        black_and_white_image = original_image1.convert('L')
 
-        # Construire le chemin de destination pour l'image en noir et blanc
+def apply_black_and_white(image_obj):
+    with open(image_obj.original_image1.path, 'rb') as f1, open(image_obj.original_image2.path, 'rb') as f2:
+        original_image1 = Image.open(f1)
+        original_image2 = Image.open(f2)
+
+        # Convertir les deux images en noir et blanc
+        black_and_white_image1 = original_image1.convert('L')
+        black_and_white_image2 = original_image2.convert('L')
+
+        # Construire le chemin de destination pour les images en noir et blanc
         destination_dir = os.path.join(settings.MEDIA_ROOT, 'bw')
         os.makedirs(destination_dir, exist_ok=True)
 
-        # Extraire le nom du fichier original
-        original_image_name = os.path.basename(image_obj.original_image1.name)
+        # Extraire le nom du fichier original pour les deux images
+        original_image1_name = os.path.basename(image_obj.original_image1.name)
+        original_image2_name = os.path.basename(image_obj.original_image2.name)
 
-        # Construire le chemin complet du fichier en noir et blanc
-        black_and_white_image_path = os.path.join(destination_dir, 'bw_' + original_image_name)
+        # Construire les chemins complets pour les images en noir et blanc
+        black_and_white_image1_path = os.path.join(destination_dir, 'bw_' + original_image1_name)
+        black_and_white_image2_path = os.path.join(destination_dir, 'bw_' + original_image2_name)
 
-        # Enregistrer l'image en noir et blanc
-        black_and_white_image.save(black_and_white_image_path)
+        # Enregistrer les images en noir et blanc
+        black_and_white_image1.save(black_and_white_image1_path)
+        black_and_white_image2.save(black_and_white_image2_path)
 
-        # Enregistrer le chemin de l'image en noir et blanc dans le modèle
-        image_obj.black_and_white_image = 'bw/bw_' + original_image_name
+        # Enregistrer les chemins des images en noir et blanc dans le modèle
+        image_obj.black_and_white_image1 = 'bw/bw_' + original_image1_name
+        image_obj.black_and_white_image2 = 'bw/bw_' + original_image2_name
+        image_obj.save()
+
+def apply_resize(image_obj, width, height):
+    with open(image_obj.original_image1.path, 'rb') as f1, open(image_obj.original_image2.path, 'rb') as f2:
+        original_image1 = Image.open(f1)
+        original_image2 = Image.open(f2)
+
+        # Redimensionner les deux images
+        resized_image1 = original_image1.resize((width, height))
+        resized_image2 = original_image2.resize((width, height))
+
+        # Construire le chemin de destination pour les images redimensionnées
+        destination_dir = os.path.join(settings.MEDIA_ROOT, 'resized')
+        os.makedirs(destination_dir, exist_ok=True)
+
+        # Extraire le nom du fichier original pour les deux images
+        original_image1_name = os.path.basename(image_obj.original_image1.name)
+        original_image2_name = os.path.basename(image_obj.original_image2.name)
+
+        # Construire les chemins complets pour les images redimensionnées
+        resized_image1_path = os.path.join(destination_dir, 'resized_' + original_image1_name)
+        resized_image2_path = os.path.join(destination_dir, 'resized_' + original_image2_name)
+
+        # Enregistrer les images redimensionnées
+        resized_image1.save(resized_image1_path)
+        resized_image2.save(resized_image2_path)
+
+        # Enregistrer les chemins des images redimensionnées dans le modèle
+        image_obj.resized_image1 = 'resized/' + os.path.basename(resized_image1_path)
+        image_obj.resized_image2 = 'resized/' + os.path.basename(resized_image2_path)
         image_obj.save()
 
 
-def apply_resize(image, width, height):
-    img = Image.open(image.original_image1.path)
-    resized_image = img.resize((width, height))
-
-    # Construire le chemin de destination pour l'image redimensionnée
-    destination_dir = os.path.join(settings.MEDIA_ROOT, 'resized')
-    os.makedirs(destination_dir, exist_ok=True)
-
-    # Extraire le nom du fichier original
-    original_image_name = os.path.basename(image.original_image1.name)
-
-    # Construire le chemin complet du fichier redimensionné
-    resized_image_path = os.path.join(destination_dir, 'resized_' + original_image_name)
-
-    # Enregistrer l'image redimensionnée
-    resized_image.save(resized_image_path)
-
-    # Enregistrer le chemin de l'image redimensionnée dans le modèle
-    image.resized_image = 'resized/resized_' + original_image_name
-    image.save()
-
 def apply_grayscale_filter(image_obj, filter_type):
-    with open(image_obj.original_image1.path, 'rb') as f:
-        original_image1 = Image.open(f)
-        grayscale_image = nuance_de_gris(original_image1, ImageFilter.SHARPEN)
+    with open(image_obj.original_image1.path, 'rb') as f1, open(image_obj.original_image2.path, 'rb') as f2:
+        original_image1 = Image.open(f1)
+        original_image2 = Image.open(f2)
 
-        # Construire le chemin de destination pour l'image en nuances de gris
+        # Appliquer un filtre de couleur à l'image
+        if filter_type == 'BLUR':
+            grayscale_image1 = original_image1.filter(ImageFilter.BLUR)
+            grayscale_image2 = original_image2.filter(ImageFilter.BLUR)
+        elif filter_type == 'CONTOUR':
+            grayscale_image1 = original_image1.filter(ImageFilter.CONTOUR)
+            grayscale_image2 = original_image2.filter(ImageFilter.CONTOUR)
+        elif filter_type == 'DETAIL':
+            grayscale_image1 = original_image1.filter(ImageFilter.DETAIL)
+            grayscale_image2 = original_image2.filter(ImageFilter.DETAIL)
+        elif filter_type == 'EDGE_ENHANCE':
+            grayscale_image1 = original_image1.filter(ImageFilter.EDGE_ENHANCE)
+            grayscale_image2 = original_image2.filter(ImageFilter.EDGE_ENHANCE)
+        elif filter_type == 'EMBOSS':
+            grayscale_image1 = original_image1.filter(ImageFilter.EMBOSS)
+            grayscale_image2 = original_image2.filter(ImageFilter.EMBOSS)
+        elif filter_type == 'FIND_EDGES':
+            grayscale_image1 = original_image1.filter(ImageFilter.FIND_EDGES)
+            grayscale_image2 = original_image2.filter(ImageFilter.FIND_EDGES)
+        elif filter_type == 'SHARPEN':
+            grayscale_image1 = original_image1.filter(ImageFilter.SHARPEN)
+            grayscale_image2 = original_image2.filter(ImageFilter.SHARPEN)
+        else:
+            grayscale_image1 = original_image1
+            grayscale_image2 = original_image2
+
+        # Construire le chemin de destination pour les images en niveaux de gris
         destination_dir = os.path.join(settings.MEDIA_ROOT, 'grayscale')
         os.makedirs(destination_dir, exist_ok=True)
 
-        # Extraire le nom du fichier original
-        original_image_name = os.path.basename(image_obj.original_image1.name)
+        # Extraire le nom du fichier original pour les deux images
+        original_image1_name = os.path.basename(image_obj.original_image1.name)
+        original_image2_name = os.path.basename(image_obj.original_image2.name)
 
-        # Construire le chemin complet du fichier en nuances de gris
-        grayscale_image_path = os.path.join(destination_dir, 'grayscale_' + original_image_name)
+        # Construire les chemins complets pour les images en niveaux de gris
+        grayscale_image1_path = os.path.join(destination_dir, 'grayscale_' + original_image1_name)
 
-        # Enregistrer l'image en nuances de gris
-        grayscale_image.save(grayscale_image_path)
+        grayscale_image2_path = os.path.join(destination_dir, 'grayscale_' + original_image2_name)
 
-        # Enregistrer le chemin de l'image en nuances de gris dans le modèle
-        image_obj.grayscale_image = 'grayscale/grayscale_' + original_image_name
+        # Enregistrer les images en niveaux de gris
+        grayscale_image1.save(grayscale_image1_path)
+        grayscale_image2.save(grayscale_image2_path)
+
+        # Enregistrer les chemins des images en niveaux de gris dans le modèle
+        image_obj.grayscale_image1 = 'grayscale/grayscale_' + original_image1_name
+        image_obj.grayscale_image2 = 'grayscale/grayscale_' + original_image2_name
         image_obj.save()
-
-def nuance_de_gris(image, filtre):
-    return image.convert("L", palette=Image.ADAPTIVE, colors=256).filter(filtre)
 
 def merge_images(image_obj):
     with open(image_obj.original_image1.path, 'rb') as f1, open(image_obj.original_image2.path, 'rb') as f2:
